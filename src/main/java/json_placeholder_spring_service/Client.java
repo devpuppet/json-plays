7901379d3e1986d33495ef6b4fb1acd84bbccb03
Qkiz288 <kkukielka@griddynamics.com>
2019-06-16 14:55:06 +0200

    removed unnnecessary SSL fix for RestTemplate

--- src/main/java/json_placeholder_spring_service/Client.java
+++ src/main/java/json_placeholder_spring_service/Client.java
@@ -1,14 +1,7 @@
 package json_placeholder_spring_service;
 
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 
 public class Client {
 
@@ -18,21 +11,7 @@ public class Client {
     public Client() {
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-
-        try {
-            SSLContext context = SSLContext.getInstance("TLSv1.2");
-            context.init(null, null, null);
-            CloseableHttpClient httpClient = HttpClientBuilder.create()
-                    .setSSLContext(context).build();
-            HttpComponentsClientHttpRequestFactory factory
-                    = new HttpComponentsClientHttpRequestFactory(httpClient);
-            restTemplate = new RestTemplate(factory);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        }
-
+        restTemplate = new RestTemplate();
     }
 
     public <ReqType, ResType> ResponseEntity<ResType> sendRequest(
